2496eb6ab7ac2de9b2f93e014b3018f83a0faca6
sam-yng <young142001@hotmail.com>
2023-01-25 14:30:23 +1100

    toggle implem

--- static/chess.js
+++ static/chess.js
@@ -24,7 +24,7 @@ const blackKing = '♚';
 const whiteQueen = '♕';
 const blackQueen = '♛';
 
-const positions = [
+const position = [
     ['81', '82', '83', '84', '85', '86', '87', '88'],
     ['71', '72', '73', '74', '75', '76', '77', '78'],
     ['61', '62', '63', '64', '65', '66', '67', '68'],
@@ -36,31 +36,65 @@ const positions = [
 ]
 
 const populateBoard = (piece, position) => {
-    document.getElementById(position).innerText = piece;
+    document.getElementById(position).innerHTML = piece;
 }
 
 for (i = 0; i < 8; i++) {
-    populateBoard(whitePawn, positions[6][i]);
+    populateBoard(whitePawn, position[6][i]);
 }
 
 for (i = 0; i < 8; i++) {
-    populateBoard(blackPawn, positions[1][i]);
+    populateBoard(blackPawn, position[1][i]);
 }
 
-populateBoard(whiteRook, positions[7][0]);
-populateBoard(whiteRook, positions[7][7]);
-populateBoard(whiteKnight, positions[7][1]);
-populateBoard(whiteKnight, positions[7][6]);
-populateBoard(whiteBishop, positions[7][2]);
-populateBoard(whiteBishop, positions[7][5]);
-populateBoard(whiteQueen, positions[7][3]);
-populateBoard(whiteKing, positions[7][4]);
+populateBoard(whiteRook, position[7][0]);
+populateBoard(whiteRook, position[7][7]);
+populateBoard(whiteKnight, position[7][1]);
+populateBoard(whiteKnight, position[7][6]);
+populateBoard(whiteBishop, position[7][2]);
+populateBoard(whiteBishop, position[7][5]);
+populateBoard(whiteQueen, position[7][3]);
+populateBoard(whiteKing, position[7][4]);
 
-populateBoard(blackRook, positions[0][0]);
-populateBoard(blackRook, positions[0][7]);
-populateBoard(blackKnight, positions[0][1]);
-populateBoard(blackKnight, positions[0][6]);
-populateBoard(blackBishop, positions[0][2]);
-populateBoard(blackBishop, positions[0][5]);
-populateBoard(blackQueen, positions[0][3]);
-populateBoard(blackKing, positions[0][4]);
+populateBoard(blackRook, position[0][0]);
+populateBoard(blackRook, position[0][7]);
+populateBoard(blackKnight, position[0][1]);
+populateBoard(blackKnight, position[0][6]);
+populateBoard(blackBishop, position[0][2]);
+populateBoard(blackBishop, position[0][5]);
+populateBoard(blackQueen, position[0][3]);
+populateBoard(blackKing, position[0][4]);
+
+let squares = [];
+
+for (i = 0; i <= 7; i++) {
+    for (j = 0; j <= 7; j++) {
+        squares.push(document.getElementById(position[i][j]));
+    }
+};
+
+for (i = 0; i < 64; i++) {
+    if (squares[i].innerHTML) {
+        squares[i].classList.add('occupied');
+    } else {
+        squares[i].classList.remove('occupied')
+    }
+};
+
+const toggle = () => {
+    document.querySelectorAll('.occupied').forEach(item => {
+        item.addEventListener('click', event => {
+            unToggle();
+            item.classList.add('toggled')
+        })
+    })
+};
+
+const unToggle = () => {
+    document.querySelectorAll('.toggled').forEach(item => {
+        item.classList.remove("toggled");
+    })
+};
+
+setInterval(toggle, 1100);
+

492d365cb115f84d3531e043676f9a21196468c4
sam-yng <young142001@hotmail.com>
2023-02-05 12:06:00 +1100

    update

--- static/chess.js
+++ static/chess.js
@@ -1,20 +1,107 @@
+class ChessPiece {
+    constructor(name, position, color) {
+        this.name = name,
+        this.position = position
+        this.color = color
+    }
+    
+}
+
+class Pawn extends ChessPiece {
+    constructor(name, position, color) {
+        super(name, position, color)
+    }
+    getPiece() {
+        return (this.color === 'black' ? '♟' : '♙')
+    }
+    getMoves() {
+        if (Game.firstMove === true) {
+            return (this.color === 'white' ? 
+            (parseInt(this.position.toString()) + 10).toString() &&
+            (parseInt(this.position.toString()) + 20).toString() : 
+            (parseInt(this.position.toString()) - 10).toString() &&
+            (parseInt(this.position.toString()) - 20).toString())
+        } else {
+            return (this.color === 'white' ? 
+            (parseInt(this.position.toString()) + 10).toString() : 
+            (parseInt(this.position.toString()) - 10).toString())
+        }
+    }
+}
+
+ class Rook extends ChessPiece {
+    constructor(name, position, color) {
+        super(name, position, color)
+    }
+     getPiece() {
+        return (this.color === 'black' ? '♜' : '♖')
+    }
+    getMoves() {
+        
+    }
+ }
+
+ class Knight extends ChessPiece {
+    constructor(name, position, color) {
+        super(name, position, color)
+    }
+     getPiece() {
+        return (this.color === 'black' ? '♞' : '♘')
+    }
+    getMoves() {
+        
+    }
+ }
+
+class Bishop extends ChessPiece {
+    constructor(name, position, color) {
+        super(name, position, color)
+    }
+     getPiece() {
+        return (this.color === 'black' ? '♝' : '♗')
+    }
+    getMoves() {
+        
+    }
+}
+
+class Queen extends ChessPiece {
+    constructor(name, position, color) {
+        super(name, position, color)
+    }
+     getPiece() {
+        return (this.color === 'black' ? '♛' : '♕')
+    }
+    getMoves() {
+        
+    }
+}
+
+
+class King extends ChessPiece {
+    constructor(name, position, color) {
+        super(name, position, color)
+    }
+     getPiece() {
+        return (this.color === 'black' ? '♚' : '♔')
+    }
+    getMoves() {
+        
+    }
+}
+
 class Game {
     constructor(board) {
-        this.board = board
-        this.pieces = pieces
-        this.turn = 'white'
-        this.whitePieces = ['♔', '♕', '♗', '♘', '♖', '♙']
-        this.blackPieces = ['♚', '♛', '♝', '♞', '♜', '♟']
-        this.chessBoard = [
-            '81', '82', '83', '84', '85', '86', '87', '88', 
-            '71', '72', '73', '74', '75', '76', '77', '78',
-            '61', '62', '63', '64', '65', '66', '67', '68',
-            '51', '52', '53', '54', '55', '56', '57', '58',
-            '41', '42', '43', '44', '45', '46', '47', '48',
-            '31', '32', '33', '34', '35', '36', '37', '38',
-            '21', '22', '23', '24', '25', '26', '27', '28',
-            '11', '12', '13', '14', '15', '16', '17', '18'
-        ]
+        this.board = board;
+        this.firstMove = true;
+        this.boxes = document.querySelectorAll('.box');
+        this.pieces = pieces;
+        this.turn = 'white';
+        this.placePieces();
+        this.identifyPieces();
+        this.occupied = document.querySelectorAll('.occupied');
+        this.allowToggle()
+        this.toggled = document.querySelector('.toggled')
     }
 
     changeTurn() {
@@ -22,75 +109,34 @@ class Game {
             turn == 'black'
         } else if (this.turn == 'black') {
             turn == 'white'
+            this.firstMove === false
         }  
     }
 
     placePieces() {
-        for (i = 0; i < 32; i++) {
-            pieces[i].populateBoard(pieces[i])
-        }
+        this.pieces.forEach(item => {
+            document.getElementById(item.position).innerHTML = item.getPiece();
+        });
     }
 
     identifyPieces() {
-        let squares = [];
-
-        for (i = 0; i <= 63; i++) {
-            squares.push(document.getElementById(chessBoard[i]));
-        }
-
-        for (i = 0; i <= 63; i++) {
-            if (squares[i].innerHTML) {
-                squares[i].classList.add('occupied');
+        this.boxes.forEach(square => {
+            if (square.innerHTML) {
+                square.classList.add('occupied');
             } else {
-                squares[i].classList.remove('occupied')
-                squares[i].classList.remove('whitePiece')
-                squares[i].classList.remove('blackPiece')
+                square.classList.remove('occupied');
             }
-        }
-    }
+        });
+    };
 
-    whiteCheck() {
-        document.querySelectorAll('.occupied').forEach(item => {
-            for (i = 0; i < 6; i++) {
-                if (item.innerHTML == whitePieces[i]) {
-                    item.classList.add('whitePiece')
-                    item.classList.remove('occupied')
-                } 
-            }
-        })
-    }
-
-    blackCheck() {
-        document.querySelectorAll('.occupied').forEach(item => {
-            for (i = 0; i < 6; i++) {
-                if (item.innerHTML == blackPieces[i]) {
-                    item.classList.add('blackPiece')
-                    item.classList.remove('occupied')
-                } 
-            }
-        })
-    }
-
-    toggle() {
-        if (turn == 'white') {
-            document.querySelectorAll('.whitePiece').forEach(item => {
-                item.addEventListener('click', event => {
-                    this.unToggle()
-                    this.removeMove()
-                    item.classList.add('toggled')
-                    item.removeEventListener('click', event)
-                })
+    allowToggle() {
+        this.pieces.forEach(square => {
+            square.addEventListener('click', event => {
+                this.unToggle()
+                square.classList.add('toggled')
+                this.showMoves()
             })
-        } else if (turn == 'black') {
-            document.querySelectorAll('.blackPiece').forEach(item => {
-                item.addEventListener('click', event => {
-                    this.unToggle()
-                    this.removeMove()
-                    item.classList.add('toggled')
-                    item.removeEventListener('click', event)
-                })
-            })
-        }
+        })
     }
 
     unToggle() {
@@ -99,52 +145,66 @@ class Game {
         })
     }
 
-    removeMove() {
-        document.querySelectorAll('.move').forEach(item => {
-            item.classList.remove('move');
-        })
+    showMoves() {
+        document.getElementById(this.pieces.getMoves()).classList.add('move')
     }
 }
 
 
-const pieces = [
-    new whitePawn('whitePawn1', chessBoard[48]),
-    new whitePawn('whitePawn2', chessBoard[49]),
-    new whitePawn('whitePawn3', chessBoard[50]),
-    new whitePawn('whitePawn4', chessBoard[51]),
-    new whitePawn('whitePawn5', chessBoard[52]),
-    new whitePawn('whitePawn6', chessBoard[53]),
-    new whitePawn('whitePawn7', chessBoard[54]),
-    new whitePawn('whitePawn8', chessBoard[55]),
-    new blackPawn('blackPawn1', chessBoard[8]),
-    new blackPawn('blackPawn2', chessBoard[9]),
-    new blackPawn('blackPawn3', chessBoard[10]),
-    new blackPawn('blackPawn4', chessBoard[11]),
-    new blackPawn('blackPawn5', chessBoard[12]),
-    new blackPawn('blackPawn6', chessBoard[13]),
-    new blackPawn('blackPawn7', chessBoard[14]),
-    new blackPawn('blackPawn8', chessBoard[15]),
-
-    new whiteRook('whiteRook1', chessBoard[56]),
-    new whiteRook('whiteRook2', chessBoard[63]),
-    new blackRook('blackRook1', chessBoard[0]),
-    new blackRook('blackRook1', chessBoard[7]),
-
-    new whiteBishop('whiteBishop1', chessBoard[58]),
-    new whiteBishop('whiteBishop2', chessBoard[61]),
-    new blackBishop('blackBishop1', chessBoard[2]),
-    new blackBishop('blackBishop2', chessBoard[5]),
-
-    new whiteKnight('whiteKnight1', chessBoard[57]),
-    new whiteKnight('whiteKnight2', chessBoard[62]),
-    new blackKnight('blackKnight1', chessBoard[1]),
-    new blackKnight('blackKnight2', chessBoard[6]),
-
-    new whiteQueen('whiteQueen', chessBoard[59]),
-    new blackQueen('blackQueen', chessBoard[3]),
-
-    new whiteKing('whiteKing', chessBoard[60]),
-    new blackKing('blackKing', chessBoard[4])
+const board = [
+    '81', '82', '83', '84', '85', '86', '87', '88', 
+    '71', '72', '73', '74', '75', '76', '77', '78',
+    '61', '62', '63', '64', '65', '66', '67', '68',
+    '51', '52', '53', '54', '55', '56', '57', '58',
+    '41', '42', '43', '44', '45', '46', '47', '48',
+    '31', '32', '33', '34', '35', '36', '37', '38',
+    '21', '22', '23', '24', '25', '26', '27', '28',
+    '11', '12', '13', '14', '15', '16', '17', '18'
 ]
+
+const pieces = [
+    new Pawn('whitePawn1', '21', 'white'),
+    new Pawn('whitePawn2', '22', 'white'),
+    new Pawn('whitePawn3', '23', 'white'),
+    new Pawn('whitePawn4', '24', 'white'),
+    new Pawn('whitePawn5', '25', 'white'),
+    new Pawn('whitePawn6', '26', 'white'),
+    new Pawn('whitePawn7', '27', 'white'),
+    new Pawn('whitePawn8', '28', 'white'),
+
+    new Pawn('blackPawn1', '71', 'black'),
+    new Pawn('blackPawn2', '72', 'black'),
+    new Pawn('blackPawn3', '73', 'black'),
+    new Pawn('blackPawn4', '74', 'black'),
+    new Pawn('blackPawn5', '75', 'black'),
+    new Pawn('blackPawn6', '76', 'black'),
+    new Pawn('blackPawn7', '77', 'black'),
+    new Pawn('blackPawn8', '78', 'black'),
+
+    new Rook('whiteRook1', '11', 'white'),
+    new Rook('whiteRook2', '18', 'white'),
+
+    new Rook('blackRook1', '81', 'black'),
+    new Rook('blackRook1', '88', 'black'),
+
+    new Bishop('whiteBishop1', '13', 'white'),
+    new Bishop('whiteBishop2', '16', 'white'),
+
+    new Bishop('blackBishop1', '83', 'black'),
+    new Bishop('blackBishop2', '86', 'black'),
+
+    new Knight('whiteKnight1', '12', 'white'),
+    new Knight('whiteKnight2', '17', 'white'),
+
+    new Knight('blackKnight1', '82', 'black'),
+    new Knight('blackKnight2', '87', 'black'),
+
+    new Queen('whiteQueen', '14', 'white'),
+    new Queen('blackQueen', 84, 'black'),
+
+    new King('whiteKing', '15', 'white'),
+    new King('blackKing', '85', 'black')
+]
+
 
 const play = new Game(board)
